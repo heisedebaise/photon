b328e41b3df4b2452c159ac3995e469593183af9
heisedebaise <heisedebaise@hotmail.com>
2020-03-22 16:51:58 +0800

    dev

--- bin/push.sh
+++ bin/push.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 git add bin
-git add **/src
-git add **/pom.xml
-git add **/*.md
 git add LICENSE
 git add pom.xml
-git add *.md
+git add README.md
+
+for name in photon-*
+do
+  git add $name/src
+  git add $name/pom.xml
+done
+
 git commit -m dev
 git push
