a3281f359c54249a50f3acb31016e7ac76da7b32
heisedebaise <heisedebaise@hotmail.com>
2020-09-18 08:40:04 +0800

    dev

---
+++ photon-ctrl/src/main/java/org/lpw/photon/ctrl/CtrlHelper.java
@@ -0,0 +1,14 @@
+package org.lpw.photon.ctrl;
+
+/**
+ * @author lpw
+ */
+public interface CtrlHelper {
+    /**
+     * 获取对外服务URL。
+     *
+     * @param uri 服务URI。
+     * @return 对外服务URL，如果未配置根服务则返回null。
+     */
+    String url(String uri);
+}
