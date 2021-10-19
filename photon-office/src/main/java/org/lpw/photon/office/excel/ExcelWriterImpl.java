f3bdd47f55a067c687b532f816b6c922408a4cd6
heisedebaise <heisedebaise@hotmail.com>
2021-10-19 15:44:01 +0800

    dev

--- photon-office/src/main/java/org/lpw/photon/office/excel/ExcelWriterImpl.java
+++ photon-office/src/main/java/org/lpw/photon/office/excel/ExcelWriterImpl.java
@@ -1,7 +1,12 @@
 package org.lpw.photon.office.excel;
 
+import java.io.OutputStream;
+
+import javax.inject.Inject;
+
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -9,9 +14,6 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.lpw.photon.util.Logger;
 import org.springframework.stereotype.Component;
-
-import javax.inject.Inject;
-import java.io.OutputStream;
 
 @Component("photon.office.excel.writer")
 public class ExcelWriterImpl implements ExcelWriter {
@@ -24,7 +26,8 @@ public class ExcelWriterImpl implements ExcelWriter {
             JSONArray sheets = object.getJSONArray("sheets");
             for (int sheetIndex = 0, sheetSize = sheets.size(); sheetIndex < sheetSize; sheetIndex++) {
                 JSONObject sheetJson = sheets.getJSONObject(sheetIndex);
-                Sheet sheet = workbook.createSheet(sheetJson.containsKey("name") ? sheetJson.getString("name") : ("sheet " + sheetIndex));
+                Sheet sheet = workbook.createSheet(
+                        sheetJson.containsKey("name") ? sheetJson.getString("name") : ("sheet " + sheetIndex));
                 int firstRow = sheetJson.containsKey("first") ? sheetJson.getIntValue("first") : 0;
                 JSONArray rows = sheetJson.getJSONArray("rows");
                 for (int rowIndex = 0, rowSize = rows.size(); rowIndex < rowSize; rowIndex++) {
